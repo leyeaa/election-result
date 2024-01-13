c9a0e4706129bea1e59fc3d9cc7d6d846b530b91
leyeaa <62375309+leyeaa@users.noreply.github.com>
2024-01-13 14:33:13 +0100

    Add files via upload

---
+++ refresh/ref_page.php
@@ -0,0 +1,55 @@
+<?php 
+//include("../db/db.php");
+include("../validate/phone_validate.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="refresh" content="1 ; http:ref_page.php">
+<title>REFRESH PAGE</title>
+<style type="text/css">
+body,td,th {
+	font-family: Tahoma, Geneva, sans-serif;
+	font-size: 30px;
+	color: #F00;
+}
+</style>
+</head>
+
+<body>
+<p>&nbsp;</p>
+<table width="100%" cellpadding="5">
+  <tr>
+    <td width="18%">&nbsp;</td>
+    <td width="67%" align="right">[ Close ]</td>
+    <td width="15%">&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>Note!!!</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td align="center">PLEASE DO NOT CLOSE THIS PAGE</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td align="center">TILL THE END OF THIS ELECTION</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td align="center">&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td align="center">&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
